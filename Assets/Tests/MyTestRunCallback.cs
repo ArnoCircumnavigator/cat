cc674feb808a10f555b0e3ce700b66a1c7aba482
Arno <1961925755@qq.com>
2023-04-14 18:00:15 +0800

    -

---
+++ Assets/Tests/MyTestRunCallback.cs
@@ -0,0 +1,35 @@
+//using NUnit.Framework.Interfaces;
+//using UnityEngine;
+//using UnityEngine.TestRunner;
+
+//[assembly: TestRunCallback(typeof(MyTestRunCallback))]
+
+//public class MyTestRunCallback : ITestRunCallback
+//{
+//    public void RunStarted(ITest testsToRun)
+//    {
+
+//    }
+
+//    public void RunFinished(ITestResult testResults)
+//    {
+//        // 所有测试都已经完成
+//        //Debug.Log("RuningFinish");
+//    }
+
+//    public void TestStarted(ITest test)
+//    {
+
+//    }
+
+//    public void TestFinished(ITestResult result)
+//    {
+//        if (result.Test.IsSuite)
+//            return;
+
+//        if (result.ResultState.Status == TestStatus.Passed)
+//            return;
+
+//        Debug.Log($"Result of {result.Name}: {result.ResultState.Status}");
+//    }
+//}
